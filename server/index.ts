802da108c1a68f2287ccd7b8d89f798d3fd334fc
Matthew Foley <matthewfoley333@gmail.com>
2025-11-28 11:57:15 +1300

    bind the server to localhost (0.0.0.0)

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import server from './server.ts'
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log('Server listening on port', PORT)
 })
